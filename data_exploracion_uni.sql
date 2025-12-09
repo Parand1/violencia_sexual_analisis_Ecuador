eee5e19309492805f5caa08b7156a03c54f02b06
Pablo Calva <pacom33@gmail.com>
2025-12-09 17:12:29 +0000

    Se añade conteo de casos de violencia

--- data_exploracion_uni.sql
+++ data_exploracion_uni.sql
@@ -1,3 +1,81 @@
+--CONTEO DE CASOS DE VIOLENCIA MAS FRECUENTES
+WITH DatosUnificados AS (
+       -- Año 2019
+       SELECT
+         REPLACE(cie10_codigo, '.', '') AS cie10_codigo -- Aseguramos formato sin puntos (ej. T742)
+       FROM `airy-runway-450418-q9.warehouse.egresosnor_2019`
+       WHERE cie10_codigo LIKE 'T74%'
+    
+       UNION ALL
+    
+      -- Año 2020
+      SELECT
+        REPLACE(cie10_codigo, '.', '') AS cie10_codigo
+      FROM `airy-runway-450418-q9.warehouse.egresosnor_2020`
+      WHERE cie10_codigo LIKE 'T74%'
+   
+      UNION ALL
+   
+      -- Año 2021 (Manejo Especial: Mapeo de descripciones a códigos)
+      SELECT
+        CASE
+          WHEN cie10_codigo LIKE 'Abuso sexual%' THEN 'T742'
+          WHEN cie10_codigo LIKE 'Abuso físico%' THEN 'T741'
+          WHEN cie10_codigo LIKE 'Abuso psicológico%' THEN 'T743'
+          WHEN cie10_codigo LIKE 'Negligencia%' OR cie10_codigo LIKE 'Abandono%' THEN 'T740'
+          WHEN cie10_codigo LIKE 'Otro maltrato%' THEN 'T748'
+          ELSE 'T749' -- Asumimos el resto como no especificado dentro del contexto de violencia
+        END AS cie10_codigo
+      FROM `airy-runway-450418-q9.warehouse.egresosnor_2021`
+      WHERE
+        cie10_codigo LIKE 'Abuso%'
+        OR cie10_codigo LIKE 'Negligencia%'
+        OR cie10_codigo LIKE 'Maltrato%'
+   
+      UNION ALL
+   
+      -- Año 2022
+      SELECT
+        REPLACE(cie10_codigo, '.', '') AS cie10_codigo
+      FROM `airy-runway-450418-q9.warehouse.egresosnor_2022`
+      WHERE cie10_codigo LIKE 'T74%'
+   
+      UNION ALL
+   
+      -- Año 2023
+      SELECT
+        REPLACE(cie10_codigo, '.', '') AS cie10_codigo
+      FROM `airy-runway-450418-q9.warehouse.egresosnor_2023`
+      WHERE cie10_codigo LIKE 'T74%'
+   
+      UNION ALL
+   
+      -- Año 2024
+      SELECT
+        REPLACE(cie10_codigo, '.', '') AS cie10_codigo
+      FROM `airy-runway-450418-q9.warehouse.egresosnor`
+      WHERE cie10_codigo LIKE 'T74%'
+    )
+   
+    SELECT
+      cie10_codigo,
+      CASE
+        WHEN cie10_codigo = 'T742' THEN 'Abuso sexual'
+        WHEN cie10_codigo = 'T741' THEN 'Abuso físico'
+        WHEN cie10_codigo = 'T740' THEN 'Negligencia o abandono'
+        WHEN cie10_codigo = 'T743' THEN 'Abuso psicológico'
+        WHEN cie10_codigo = 'T748' THEN 'Otros síndromes de maltrato'
+        WHEN cie10_codigo = 'T749' THEN 'Síndrome del maltrato, no especificado'
+        ELSE 'Otro'
+      END AS descripcion_diagnostico,
+      COUNT(*) AS total_casos_acumulados,
+      ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 2) AS porcentaje_del_total
+    FROM DatosUnificados
+    GROUP BY cie10_codigo
+    ORDER BY total_casos_acumulados DESC;
+
+
+
 -- CONTEO DE CASOS DE ABUSO SEXUAL POR AÑO
 SELECT
          anio,
